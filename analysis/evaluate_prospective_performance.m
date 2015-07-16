c934cf4bae790ad5df67dee0c9ebdb54ae3c86ab
surgebiswas <surgebiswas@gmail.com>
2015-07-16 15:08:27 +0100

    evaluate_prospective_performance bug fix
    Added standardization of ytrain before marker_OMP

--- analysis/evaluate_prospective_performance.m
+++ analysis/evaluate_prospective_performance.m
@@ -43,7 +43,7 @@ end
 % necessary.
 if decompositionNeeded 
     fprintf('Performing OMP decomposition ... ');
-    somp = marker_OMP(ytrain, params.PROPUNEXPVAR, 'maxfeats', params.MAXFEATS, ...
+    somp = marker_OMP(standardize(ytrain), params.PROPUNEXPVAR, 'maxfeats', params.MAXFEATS, ...
         'savememory', params.SAVEMEMORY);
     save(saveFile, 'somp', 'params');
     fprintf('Done.\n');
