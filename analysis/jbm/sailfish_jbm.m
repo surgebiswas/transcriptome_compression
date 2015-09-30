ac38e13e12f3af32c960d4437401974a9b17c0b6
surgebiswas <surgebiswas@gmail.com>
2015-09-30 18:50:55 -0400

    sailfish_jbm now live

--- analysis/jbm/sailfish_jbm.m
+++ analysis/jbm/sailfish_jbm.m
@@ -6,7 +6,7 @@ for i = 1 : length(d.Var1)
     % Copy the file
     cmd = sprintf('cp %s .', d.Var1{i});
     fprintf('%s\n', cmd);
-    %system(cmd);
+    system(cmd);
     
     [p,f,e] = fileparts(d.Var1{i});
     fname = strrep(d.Var1{i}, [p, '/'], '');
@@ -15,23 +15,23 @@ for i = 1 : length(d.Var1)
     % Gunzip
     cmd = sprintf('gunzip %s', fname);
     fprintf('%s\n', cmd);
-    %system(cmd);
+    system(cmd);
     
     
     % Sailfish
     splits = regexpi(fname, '_', 'split');
     outdir = [splits{1}, '_', splits{2}];
-    %mkdir(outdir);
+    mkdir(outdir);
     cmd = sprintf('sailfish quant -i %s -l ''T=SE:S=U'' -r %s -o %s -p 6 &> %s', ...
         '/proj/dangl_lab/sbiswas/sailfish_indexes/tair10', fnamefasta, outdir, [outdir, '/sf.out']);
     bcmd = sprintf('bsub -q day -M 30 -o %s -e %s -n 6 -R "span[hosts=1]" "%s"', ...
         [outdir, '/lsf.out'], [outdir, '/lsf.err'], cmd);
     fprintf('%s\n', bcmd);
-    %system(cmd);
+    system(cmd);
     
     cmd = sprintf('mv %s %s', fnamefasta, outdir);
     fprintf('%s\n', cmd);
-    %system(cmd);
+    system(cmd);
     
     return;
 end
