758ea25fb16306fc45da5f532819cdf49685c920
surgebiswas <surgebiswas@gmail.com>
2015-09-30 18:42:29 -0400

    minor

--- analysis/jbm/sailfish_jbm.m
+++ analysis/jbm/sailfish_jbm.m
@@ -21,12 +21,12 @@ for i = 1 : length(d.Var1)
     % Sailfish
     splits = regexpi(fname, '_', 'split');
     outdir = [splits{1}, '_', splits{2}];
-    mkdir(outdir);
+    %mkdir(outdir);
     cmd = sprintf('sailfish quant -i %s -l ''T=SE:S=U'' -r %s -o %s -p 6 &> %s', ...
         '/proj/dangl_lab/sbiswas/sailfish_indexes/tair10', fnamefasta, outdir, [outdir, '/sf.out']);
     bcmd = sprintf('bsub -q day -M 30 -o %s -e %s -n 6 -R "span[hosts=1]" %s', ...
         [outdir, '/lsf.out'], [outdir, '/lsf.err'], cmd);
-    fprintf('%s\n', cmd);
+    fprintf('%s\n', bcmd);
     %system(cmd);
 end
 
